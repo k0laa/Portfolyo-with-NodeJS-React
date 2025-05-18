b9535e9b2a40bc8575f5b559e3db98f97d9aaa58
Buğra Yalçın <bugrayalcn1@gmail.com>
2025-05-18 16:32:51 +0300

    sayfalar arası geçişte scroll özelliği hatası düzeltildi.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {ThemeProvider, Box, CssBaseline} from '@mui/material';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import emailjs from '@emailjs/browser';
 import {EMAILJS_CONFIG} from './config/constants';
 import {getTheme} from './config/theme';
@@ -15,6 +15,24 @@ import Timeline from "./components/Timeline";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import AllProjects from './pages/AllProjects';
+
+// Scroll to hash component
+const ScrollToHash = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.hash) {
+            const element = document.querySelector(location.hash);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        } else {
+            window.scrollTo(0, 0);
+        }
+    }, [location]);
+
+    return null;
+};
 
 function App() {
     const [isDarkMode, setIsDarkMode] = useState(true);
@@ -32,6 +50,7 @@ function App() {
             <CssBaseline/>
             <Router>
                 <Box className="App" sx={{width: '100%', overflowX: 'hidden', position: 'relative'}}>
+                    <ScrollToHash />
                     <Navbar toggleTheme={toggleTheme} isDarkMode={isDarkMode}/>
                     <Routes>
                         <Route path="/" element={
